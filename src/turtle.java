72b1305adf205b4385c8eeeebdc212c22955591b
sameh12468 <amrz2011@live.com>
2019-04-28 23:22:16 +0200

    first commit

---
+++ src/turtle.java
@@ -0,0 +1,30 @@
+
+public class turtle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		import turtle 
+
+		ninja = turtle.Turtle()
+
+		ninja.speed(10)
+
+		for i in range(180):
+		    ninja.forward(100)
+		    ninja.right(30)
+		    ninja.forward(20)
+		    ninja.left(60)
+		    ninja.forward(50)
+		    ninja.right(30)
+		    
+		    ninja.penup()
+		    ninja.setposition(0, 0)
+		    ninja.pendown()
+		    
+		    ninja.right(2)
+		    
+		turtle.done()
+
+	}
+
+}
